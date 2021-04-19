4976579076eba3aa822ecbe268d5efec354bfe41
Csaba Toth <csaba.toth.work@protonmail.com>
2021-04-19 22:45:48 +0200

    speedConvert

--- src/ExpressionsStatementsCodeblocksMethods/Methods.java
+++ src/ExpressionsStatementsCodeblocksMethods/Methods.java
@@ -19,6 +19,11 @@ public class Methods {
 
         calculateScore(true, 800, 5, 100);
         calculateScore(true, 10000, 8, 200);
+
+        displayHighScorePosition("John", calculateHighScorePosition(1500));
+        displayHighScorePosition("Ian", calculateHighScorePosition(900));
+        displayHighScorePosition("Peter", calculateHighScorePosition(400));
+        displayHighScorePosition("Ema", calculateHighScorePosition(50));
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
@@ -28,5 +33,33 @@ public class Methods {
             return finalScore;
         }
         return -1;
+    }
+
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + " managed to get into position " + position + ". on the high score table.");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500) {
+            return 2;
+        } else if (score >= 100) {
+            return 3;
+        }
+        return 4;
+    }
+    public static int calculateHighScorePosition2 (int score) {  // alternative way to do the same
+        int position = 4; // assuming position 4 will be returned
+
+        if (score >= 1000) {
+            return 1;
+        } else if (score >= 500) {
+            return 2;
+        } else if (score  >= 100) {
+            return 3;
+        }
+
+        return position;
     }
 }
