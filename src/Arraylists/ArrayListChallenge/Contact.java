21ea05eb73d2180661aa477a69537327d912b310
Csaba Toth <csaba.toth.work@protonmail.com>
2021-04-29 19:40:05 +0200

    arraylists

---
+++ src/Arraylists/ArrayListChallenge/Contact.java
@@ -0,0 +1,33 @@
+package Arraylists.ArrayListChallenge;
+
+public class Contact {
+    private String name;
+    private int phoneNumber;
+
+    public Contact(String name, int phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "name=" + name + '\'' +
+                ", phoneNumber=" + phoneNumber;
+    }
+}
---
+++ src/Arraylists/ArrayListChallenge/Main.java
@@ -0,0 +1,9 @@
+package Arraylists.ArrayListChallenge;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MobilePhone mobilePhone = new MobilePhone();
+        mobilePhone.showMenu();
+    }
+}
